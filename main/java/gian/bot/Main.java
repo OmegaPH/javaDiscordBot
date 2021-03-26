f20c2bb9cd144e3ca2352b59d76f5dd4273cd5cf
Gcs <carlogian1834@gmail.com>
2021-03-27 01:40:24 +0800

    first commit

---
+++ main/java/gian/bot/Main.java
@@ -0,0 +1,16 @@
+package gian.bot;
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        DiscordApi Bot = new DiscordApiBuilder().setToken("ODI0Njc4NDYzMzkwMzUxNDEw.YFy3qw.QHCOgV7xcASxHMmeWDztwEohBE0").login().join();
+
+
+
+    }
+
+}
